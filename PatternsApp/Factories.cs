890a9594185dc67064840662b5f744ff217b95b4
Eugene <Eugene@DESKTOP-LTEFHQB>
2024-08-19 01:12:48 +0300

    Factories add

---
+++ PatternsApp/Factories.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PatternsApp
+{
+    public class FactoryPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        //public List<FactoryPerson> FactoryList = new List<FactoryPerson>();
+        public FactoryPerson[] FactoryList = new FactoryPerson[0];
+
+        public FactoryPerson CreatePerson(string name)
+        {
+            int personsCount = FactoryList.Length;
+            FactoryPerson factoryPerson = personsCount != 0 ? new FactoryPerson() { Name = name, Id = personsCount } : new FactoryPerson() { Name = name, Id = 0 };
+            var temt = FactoryList;
+            FactoryList = new FactoryPerson[personsCount + 1];
+            for (int i = 0; i < temt.Length; i++)
+            {
+                FactoryList[i] = temt[i];
+            }
+            FactoryList[personsCount] = factoryPerson;
+            return factoryPerson;
+        }
+    }
+
+
+}
--- PatternsApp/Program.cs
+++ PatternsApp/Program.cs
@@ -7,14 +7,19 @@ using static PatternsApp.Person;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-   
+
 
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            
+            var pf = new PersonFactory();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var person = pf.CreatePerson("user");
+            }
 
             Console.ReadLine();
         }
